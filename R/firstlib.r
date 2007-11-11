9808f89b6dacce1926b117fb26cb87f9bbafea28
Longhai Li <longhai@math.usask.ca>
2007-11-11 00:00:00 +0000

    version 1.1-2

--- R/firstlib.r
+++ R/firstlib.r
@@ -2,7 +2,7 @@
 {
    library.dynam("predbayescor",pkg,lib)
    cat("predbayescor loaded\n", 
-       "COPY RIGHT (c) Longhai Li\n", 
-       "http://math.usask.ca/~longhai\n",sep="")
+       "COPY RIGHT (c) Longhai Li,http://math.usask.ca/~longhai\n",
+       "Type ?begin.predbayescor for help\n",sep="")
 }
 
